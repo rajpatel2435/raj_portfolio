4c3393d8c442b8b4a4a35d7a752c5d036ef8461c
Raj <rpatel5@isi-montreal.com>
2022-07-20 16:08:32 -0400

    css error solved

--- pages/About_info.tsx
+++ pages/About_info.tsx
@@ -91,7 +91,7 @@ function About_info() {
         <div className='ml-24 '>
           <div className="box">
             <div className="content ">
-              <Image src={CSS}  alt="css" className='img  rounded-full  hover:scale-90' />
+              <Image src={CSS}  alt="css"  height='200px' width='200px' className='img  rounded-full  hover:scale-90' />
               <h2 className=''>CSS<span><br />95%</span></h2>
 
               {/* </div> */}
